88d319e563ed0a080d1453e3e980b569a3271013
James Strachan <james.strachan@gmail.com>
2020-06-26 15:24:16 +0100

    fix: improve tests

--- pkg/launcher/job/job_launcher_test.go
+++ pkg/launcher/job/job_launcher_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jenkins-x/jx-git-operator/pkg/launcher"
 	"github.com/jenkins-x/jx-git-operator/pkg/launcher/job"
 	"github.com/jenkins-x/jx-git-operator/pkg/repo"
+	"github.com/jenkins-x/jx-helpers/pkg/testhelpers"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -17,14 +18,14 @@ import (
 
 func TestJobLauncher(t *testing.T) {
 	ns := "jx"
-	secretName := "my-secret"
+	repoName := "fake-repository"
 	gitURL := "https://github.com/jenkins-x/fake-repository.git"
 	gitSha := "dummysha1234"
 
 	kubeClient := fake.NewSimpleClientset(
 		&corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      secretName,
+				Name:      repoName,
 				Namespace: ns,
 				Labels: map[string]string{
 					constants.DefaultSelectorKey: constants.DefaultSelectorValue,
@@ -37,11 +38,11 @@ func TestJobLauncher(t *testing.T) {
 	)
 
 	client, err := job.NewLauncher(kubeClient, ns, constants.DefaultSelector)
-	require.NoError(t, err, "failed to create laucher client")
+	require.NoError(t, err, "failed to create launcher client")
 
 	o := launcher.LaunchOptions{
 		Repository: repo.Repository{
-			Name:      secretName,
+			Name:      repoName,
 			Namespace: ns,
 			GitURL:    gitURL,
 		},
@@ -57,4 +58,14 @@ func TestJobLauncher(t *testing.T) {
 	require.True(t, ok, "could not convert object %#v to a Job")
 
 	t.Logf("created Job with name %s", j1.Name)
+
+	msg := "created Job"
+	testhelpers.AssertLabel(t, constants.DefaultSelectorKey, constants.DefaultSelectorValue, j1.ObjectMeta, msg)
+	testhelpers.AssertLabel(t, launcher.RepositoryLabelKey, repoName, j1.ObjectMeta, msg)
+	testhelpers.AssertLabel(t, launcher.CommitShaLabelKey, gitSha, j1.ObjectMeta, msg)
+
+	// we should not recreate the Job if we try to launch again as it already exists
+	objects, err = client.Launch(o)
+	require.NoError(t, err, "failed to launch the job")
+	require.Len(t, objects, 0, "should have created one runtime.Object after launching")
 }
